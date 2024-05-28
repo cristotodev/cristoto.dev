1e06fb20078b1a6eb47d67eecaed53447a5b0623
Cristotodev <cristom.estevez@gmail.com>
2024-05-28 21:05:52 +0100

    fix: lowercase to search on title and description

--- src/pages/api/posts.json.ts
+++ src/pages/api/posts.json.ts
@@ -5,9 +5,11 @@ export async function GET({ request }: { request: Request }) {
     const searchParam = url.searchParams.get('search')?.trim();
 
     if (searchParam && searchParam.length > 0) {
+        const searchParamsLowerCase = searchParam.toLowerCase();
         const posts = (await getCollection('blog'))
-            .filter(post => post.data.title.toLocaleLowerCase().includes(searchParam) || post.data.description.toLocaleLowerCase().includes(searchParam))
+            .filter(post => post.data.title.toLocaleLowerCase().includes(searchParamsLowerCase) || post.data.description.toLocaleLowerCase().includes(searchParamsLowerCase))
             .sort((b, a) => a.data.pubDate - b.data.pubDate);
+
 
         return new Response(
             JSON.stringify({
