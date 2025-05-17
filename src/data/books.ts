d37ab6d02ca2dd74774d8a86d59a213861b936fd
Cristotodev <cristom.estevez@gmail.com>
2025-05-17 09:43:35 +0100

    feat: refered links and book details

--- src/data/books.ts
+++ src/data/books.ts
@@ -1,32 +1,66 @@
-export const books: { title: string, author: string, cover: string }[] = [
+export const books: { id: number, title: string, author: string, cover: string, link: string, description: string }[] = [
     {
-        "title": "Clean Code",
-        "author": "Robert C. Martin",
-        "cover": "https://res.cloudinary.com/cristotodev/image/upload/v1747467103/cristotodev/books/clean-code_ifoo7u.webp"
+        id: 1,
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747467103/cristotodev/books/clean-code_ifoo7u.webp",
+        link: "https://amzn.to/45dTr51",
+        description: "Aprende a escribir código limpio, fácil de entender y mantener, siguiendo principios que mejoran la calidad y eficiencia de tus proyectos. Un clásico imprescindible para todo desarrollador que quiera sobresalir en su carrera."
     },
     {
-        "title": "The Pragmatic Programmer",
-        "author": "Andrew Hunt, David Thomas",
-        "cover": "https://res.cloudinary.com/cristotodev/image/upload/v1747467103/cristotodev/books/pragmatic-programmer_l2g2fo.webp"
+        id: 2,
+        title: "The Pragmatic Programmer",
+        author: "Andrew Hunt, David Thomas",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747467103/cristotodev/books/pragmatic-programmer_l2g2fo.webp",
+        link: "https://amzn.to/44CPF59",
+        description: "Transforma tu forma de pensar como programador con consejos prácticos que te ayudarán a ser más eficiente, adaptable y productivo. Una guía esencial para quienes buscan convertirse en desarrolladores completos y versátiles."
     },
     {
-        "title": "Código sostenible",
-        "author": "Carlos Blé Jurado",
-        "cover": "https://res.cloudinary.com/cristotodev/image/upload/v1747467335/cristotodev/books/codigo_sostenible_zkxmjx.webp"
+        id: 3,
+        title: "Código sostenible",
+        author: "Carlos Blé Jurado",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747467335/cristotodev/books/codigo_sostenible_zkxmjx.webp",
+        link: "https://amzn.to/3GXtFYS",
+        description: "Descubre cómo crear software que perdure en el tiempo, fácil de mantener y escalable. Aprende a escribir código que sea tanto limpio como sostenible para proyectos a largo plazo."
     },
     {
-        "title": "You Don't Know JS (Yet)",
-        "author": "Kyle Simpson",
-        "cover": "https://res.cloudinary.com/cristotodev/image/upload/v1747467699/cristotodev/books/you-dont-know-js_bn5xrt.webp"
+        id: 4,
+        title: "You Don't Know JS (Yet)",
+        author: "Kyle Simpson",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747467699/cristotodev/books/you-dont-know-js_bn5xrt.webp",
+        link: "https://amzn.to/3SFF6H7",
+        description: "Rompe las barreras de JavaScript y conviértete en un verdadero maestro del lenguaje. Este libro profundiza en conceptos avanzados que todo desarrollador debería dominar para crear aplicaciones web más rápidas y eficientes."
     },
     {
-        "title": "Design Patterns",
-        "author": "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
-        "cover": "https://res.cloudinary.com/cristotodev/image/upload/v1747467879/cristotodev/books/design-patterns_kx91zf.webp"
+        id: 5,
+        title: "Design Patterns",
+        author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747467879/cristotodev/books/design-patterns_kx91zf.webp",
+        link: "https://amzn.to/3Fe6JnG",
+        description: "Descubre los patrones de diseño que han revolucionado el desarrollo de software moderno. Aprende a crear arquitecturas flexibles y reutilizables para resolver problemas complejos de manera elegante y eficiente."
     },
     {
-        "title": "Hábitos Atómicos",
-        "author": "James Clear",
-        "cover": "https://res.cloudinary.com/cristotodev/image/upload/v1747468144/cristotodev/books/habitos-atomicos_rsp235.webp"
+        id: 6,
+        title: "Hábitos Atómicos",
+        author: "James Clear",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747468144/cristotodev/books/habitos-atomicos_rsp235.webp",
+        link: "https://amzn.to/3Zpi9f9",
+        description: "Transforma tu vida un hábito a la vez. Aprende cómo pequeños cambios pueden tener un impacto enorme en tu productividad, salud y felicidad a largo plazo."
     },
-]
+    {
+        id: 7,
+        title: "El Camino del Directivo",
+        author: "Camille Fournier",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747469185/cristotodev/books/camino-del-directivo_tswwph.webp",
+        link: "https://amzn.to/45goHjY",
+        description: "De desarrollador a líder técnico. Este libro te guía en el camino para convertirte en un líder efectivo, capaz de dirigir equipos y tomar decisiones estratégicas con confianza."
+    },
+    {
+        id: 8,
+        title: "El poder de confiar en ti",
+        author: "Curro Cañete",
+        cover: "https://res.cloudinary.com/cristotodev/image/upload/v1747469346/cristotodev/books/el-poder-de-confiar-en-ti_tz7syz.webp",
+        link: "https://amzn.to/4dl3mbj",
+        description: "Libérate de tus dudas y fortalece tu confianza para alcanzar tus metas personales y profesionales. Un libro inspirador para quienes buscan desbloquear su verdadero potencial."
+    }
+];
